e773975137b36ef2a5134390962b886ed070885e
Daniel Santillan <ldms99@hotmail.com>
2022-11-08 12:45:43 -0600

    project working

--- app.js
+++ app.js
@@ -1,44 +1,62 @@
-const url = 'https://staging-app.enviosperros.com/api/v2/shipping/rates';
+const postalCode = document.querySelectorAll('input[type="number"]').forEach(input => {
+  input.oninput = () => {
+    if (input.value.length > input.maxLength) input.value = input.value.slice (0, input.maxLength)
+  }
+});;
+const form = document.getElementById('form');
 
-const apiKey = 'zi7YJxULkOcUevMpMq1gGTNBbZtmLQkH1XuT2CjN';
+const userCode = document.getElementById("cp");
+const fedex = document.getElementById('parcel1');
+const dhl = document.getElementById('parcel2');
+const fedexBox = document.querySelector(".fedex");
+const dhlBox = document.querySelector(".dhl");
 
-function envios() {
-  var request = new XMLHttpRequest();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  request.open('POST', 'https://staging-app.enviosperros.com/api/v2/shipping/rates');
+  fedex.innerHTML = '';
+  dhl.innerHTML = '';
 
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.setRequestHeader('Authentication', `Bearer ${apiKey}`);
+  fetchFedexApi(userCode.value);
+  fetchDHLApi(userCode.value)
+  userCode.value = '';
+});
 
-  request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      console.log('Status:', this.status);
-      console.log('Headers:', this.getAllResponseHeaders());
-      console.log('Body:', this.responseText);
+function fetchFedexApi(code) {
+  fedexBox.classList.remove("hidden");
+
+  const url = `https://sistema.globalpaq.mx/api/v2/public/fedex/cobertura?cp_origen=44360&cp_destino=${code}`
+  fetch(url, {
+    headers: {
+      'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2Njc4NTE3OTUsImV4cCI6MTY5OTM4Nzc5NSwiZGF0YSI6eyJpZCI6NzMwMTksIm5hbWUiOiJEYW5pZWwiLCJlbWFpbCI6ImxkbXM5OUBob3RtYWlsLmNvbSIsImV4Y2VkZW50ZSI6MCwic2VndXJvIjowLCJtdWx0aSI6MCwicmVjb2xlY2Npb24iOjB9fQ.0EwaHOF_9KZhv0SXa9HkC96Vgm6Z44N4Nxg0KbLFFJ0',
     }
-  };
-
-  var body =     {
-          'api_token':apiKey,
-          'depth': '20',
-          'width': '20',
-          'height': '20',
-          'weight': '1',
-          'origin': {
-              'codePostal': '45130'
-          },
-          'destination': {
-              'codePostal': '64000'
-          },
-  };
-
-  request.send(JSON.stringify(body));
+  })
+  .then(response => response.json())
+  .then(data => {
+    const { data: { message }} = data;
+    fedex.innerHTML = message;
+    (fedex.innerText.includes('x')) ? zonaExtendida() : zonaExtendida();
+  })
 }
 
-async function getEnvios() {
-  const resultado = await envios();
+function fetchDHLApi(code) {
+  dhlBox.classList.remove("hidden");
 
-  console.log(resultado);
+  const url = `https://sistema.globalpaq.mx/api/v2/public/dhl/cobertura?cp_origen=44360&cp_destino=${code}`
+  fetch(url, {
+    headers: {
+      'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2Njc4NTE3OTUsImV4cCI6MTY5OTM4Nzc5NSwiZGF0YSI6eyJpZCI6NzMwMTksIm5hbWUiOiJEYW5pZWwiLCJlbWFpbCI6ImxkbXM5OUBob3RtYWlsLmNvbSIsImV4Y2VkZW50ZSI6MCwic2VndXJvIjowLCJtdWx0aSI6MCwicmVjb2xlY2Npb24iOjB9fQ.0EwaHOF_9KZhv0SXa9HkC96Vgm6Z44N4Nxg0KbLFFJ0',
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    const { data: { message }} = data;
+    dhl.innerHTML = message;
+    (dhl.innerText.includes('x')) ? zonaExtendida() : zonaExtendida()
+  })
 }
 
-getEnvios();
+function zonaExtendida() {
+  (fedex.innerText.includes('x')) ? fedexBox.classList.add('extendida') : fedexBox.classList.remove('extendida');
+  (dhl.innerText.includes('x')) ? dhlBox.classList.add('extendida') : dhlBox.classList.remove('extendida');
+}
